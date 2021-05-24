d50ef979721a4d576fb65816bc9d780678a8c985
Donthu120902 <48579045+Donthu120902@users.noreply.github.com>
2021-05-24 13:12:20 +0530

    Update ELk-Installsteps-Userdata.sh

--- ELk-Installsteps-Userdata.sh
+++ ELk-Installsteps-Userdata.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get install openjdk-8-jdk
+sudo apt-get update -y
+sudo apt-get install -y openjdk-8-jdk
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 sudo apt-get install apt-transport-https -y
-echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
-sudo apt-get update && sudo apt-get install elasticsearch && sudo apt-get install logstash && sudo apt-get install kibana
+sudo echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
+sudo apt-get -y update && sudo apt-get install -y elasticsearch && sudo apt-get -y install logstash && sudo apt-get -y install kibana
+sudo echo "network.host: 0.0.0.0" >> /etc/elasticsearch/elasticsearch.yml
+sudo echo "discovery.type: single-node" >> /etc/elasticsearch/elasticsearch.yml
+sudo systemctl start elasticsearch
